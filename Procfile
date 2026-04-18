297b739d1fd2ca75243bf3efc44a45b838b875ca
JmesterL <abramovvlad201@gmail.com>
2026-04-18 15:33:16 +0700

    Add files via upload

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python TelegramBot.py
+web: python Bot.py
---
+++ bot.py
@@ -0,0 +1,63 @@
+import telebot
+import os
+from telebot import types
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Бот работает!"
+
+
+def run_flask():
+    app.run(host='0.0.0.0', port=8080)
+
+threading.Thread(target=run_flask).start()
+
+
+#token bota
+TOKEN = os.environ.get('BOT_TOKEN')
+bot = telebot.TeleBot(TOKEN)
+
+#переменная клавиатура, в которой хранится эта команда
+#как я понял, в скобочках дается размер кнопок, когда правда = они маленькие, когда лож = они большие
+Keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+#А это сами кнопки, кнопки это просто слова которые не надо печатать. Если написать в ручную, то все будет работать
+#у меня knopka = knp
+knp1 = types.KeyboardButton("Помощь")
+
+#Надо добавить теперь кнопку на экран
+Keyboard.add(knp1)
+
+
+#1 функция
+#если нажата кнопка старт, то он будет печатать это
+@bot.message_handler(commands=["start"])
+def start(message):                                                                                #насильно вызывает клавиатуру во время старта
+    bot.send_message(message.chat.id, "ПРИВЕЕЕЕТ! Я бот попугай и буду повторять за тобой слова!", reply_markup=Keyboard)
+     #тут аналогично, не нужно ретурн, функция сама выполнит и напишет по айди отправителя уже заданный текст!
+
+
+
+#2 функция
+#если напечатать помощь, то он будет писать кто он и для чего. Ловер и стрип должны быть после text для правильности
+@bot.message_handler(func=lambda message: message.text.lower().strip() == "помощь")#и если я делаю кнопки, лучше значения писать с маленькой буквы
+def help(message):
+    bot.send_message(message.chat.id, "Давай введу в курс дела!\nЯ создан для чила и расслабона!\nПока я могу лишь только повторять за тобой слова\nНО! В будущем я стану куда продвинутей!")
+        #тут аналогично, не нужно ретурн, функция сама выполнит и напишет по айди отправителя уже заданный текст!
+
+
+
+
+#3 функиця
+#если он ловит любое сообщение, то вызывается основная функция попугая
+@bot.message_handler(func=lambda message:True)
+def echo(message):
+    bot.send_message(message.chat.id, f"КхмКхм... {message.text}")
+        #Это то что он будет делать, писать сообщение по айди отправителя копируя его текст с препиской кхмкхм
+
+
+print("Бот работает!")
+bot.infinity_polling()
